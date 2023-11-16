6371fa5f21a776b6825f55d6e7b0957c3ecfdc28
Robert Chandler <robert@akchandler.com>
2023-11-16 14:32:47 -0700

    Add more template styles

--- Rhema Journal/Models/Style.swift
+++ Rhema Journal/Models/Style.swift
@@ -12,6 +12,9 @@ import SwiftData
 enum Style: String, CaseIterable {
     case lectio = "Lectio Divina"
     case dbs = "Discovery Bible Study"
+    case twi = "TWI"
+    case soap = "SOAP"
+    case hear = "HEAR"
     case none = "None"
 }
 
@@ -27,6 +30,23 @@ let StylePrompts = [
         "What does this passage tell me about God?",
         "What does this story tell me about people?",
         "What is the right response in my life to this passage?",
+    ],
+    Style.twi: [
+        "Thanksgiving: Thank God for what He's done and who He is. Write down anything you are thankful for",
+        "Worship: Agree with who He is. Focus on a particular attribute of His character",
+        "Intercession: Agree with what He wants to do",
+    ],
+    Style.soap: [
+        "Scripture: Copy down or paraphrase the passage",
+        "Observation: What stands out to you?",
+        "Application: What is the right response in your life to this passage?",
+        "Prayer: Does this passage spur any prayer for yourself or someone else?",
+    ],
+    Style.hear: [
+        "Highlight: What words or phrases seem to jump out at you?",
+        "Explain: What does this passage mean to its original audience?",
+        "Apply: What does this passage mean today, to you?",
+        "Respond: How can you think or act differently in response to this passage?",
     ],
     Style.none: [
         "Notes",
